aed4e26ac54f0e2f4f37b42b6b2056f7851bbd97
emmaddo <ermsapp@gmail.com>
2024-07-09 08:35:56 +0100

    Using repeat() method for both normal upright right angle triangle and inverted right angle triangle

---
+++ invertedRightAngleTrianle-repeat-method.js
@@ -0,0 +1,9 @@
+function invertedRightAngleTrianle(height){
+    
+    for(let i = height; i >= 0; i--){
+        block ='#'.repeat(i);
+        console.log(block);
+    }
+    
+}
+invertedRightAngleTrianle(8);
---
+++ uprightRightAngleTrianle-repeat-method.js
@@ -0,0 +1,9 @@
+function uprightRightAngleTrianle(height){
+    
+    for(let i = 1; i <= height; i++){
+        block ='#'.repeat(i);
+        console.log(block);
+    }
+    
+}
+uprightRightAngleTrianle(8);
